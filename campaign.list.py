262d77a9bf5ac4cb936ad52f54f75c4527679e7e
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-03-04 09:47:17 +0100

    add campaign stat

--- campaign.list.py
+++ campaign.list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from login import p, ses
 
-res = p.campaigns.list({'session' : ses})
+res = p.campaigns.list({
+	'session' : ses,
+	'userId' : 235632},
+	)
 ses = res["session"]
 for c in res["campaigns"]:
 
