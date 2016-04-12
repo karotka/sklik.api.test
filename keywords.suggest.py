88bfef21f9891f5097a7b03a369abee7752db988
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-04-12 16:25:01 +0200

    add msg

--- keywords.suggest.py
+++ keywords.suggest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from login import p, ses
 
-res = p.keywords.suggest({'session' : ses}, 'mazda 6')
+res = p.keywords.suggest({'session' : ses}, 'mazda')
 ses = res["session"]
 for c in res["suggestions"]:
     print "Query: %s \t %s \t %s" % (c["query"], c["avgSearchCount"], c["cpc"]) 
