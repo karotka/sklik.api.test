04e628cc8822c19f068f3841e01732e501ae1358
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-06-24 13:04:26 +0200

    add

--- keywords.suggest.stats.py
+++ keywords.suggest.stats.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from login import p, ses
 
-res = p.keywords.suggest.stats({'session' : ses}, ['mazda 6'])
+res = p.keywords.suggest.stats({'session' : ses}, [
+'pneumatiky'
+
+], {"granularity" : "monthly"})
 ses = res["session"]
 for c in res["stats"]:
     print "Query: %s \t %s \t %s" % (c["query"], c["avgSearchCount"], c["avgCpc"]) 
-
+    for i in c["searchCountInTime"]:
+        print i
