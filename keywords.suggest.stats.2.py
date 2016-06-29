7d81c76ff7cde39e604de5b1156c28c840df3948
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-06-29 09:13:04 +0200

    add

---
+++ keywords.suggest.stats.2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from login import p, ses
+
+res = p.keywords.suggest.stats({'session' : ses}, [
+    "samsung galaxy s6",
+    "iphone 5",
+    "camera",
+    "drive",
+
+], {"granularity" : "monthly"})
+ses = res["session"]
+for c in res["stats"]:
+    print "Query: %s \t %s \t %s" % (c["query"], c["avgSearchCount"], c["avgCpc"]) 
+    for i in c["searchCountInTime"]:
+        print i
