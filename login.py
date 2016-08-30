a3fb81f5a1ac6285b19aef7367da8411fbe64b93
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-08-30 10:10:20 +0200

    add all

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import xmlrpclib
 
 p = xmlrpclib.ServerProxy('https://api.sklik.cz/cipisek/RPC2')
-result = p.client.login("karotka@seznam.cz", '')
+result = p.client.login("put_your_username_with_domain_here", 'put_you_passsword_here')
 
 if result["status"] == 200:
     ses = result["session"]
