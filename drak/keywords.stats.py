cb14b7f80f2749674aa2c6f743b270f3b4ae3ff7
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-09-07 10:08:24 +0200

    https://nas.sklik.cz/keywords?table=(dir:DESC,limit:250,page:1,sort:impressions)&dateRange=(from:%272016-09-03%27,to:%272016-09-03%27)&status=nondeleted&filter=()

--- drak/keywords.stats.py
+++ drak/keywords.stats.py
@@ -8,21 +8,29 @@ USER = {
         'userId' : 72972}
 
 FILTER = {
-        "statisticsConditions" : [{"columnName" : "clicks", "operator" : "GT", "intValue" : 0}],
+        "statisticsConditions" : [{"columnName" : "impressions", "operator" : "GT", "intValue" : 0}],
         "dateFrom" : datetime.strptime("03.09.2016", "%d.%m.%Y"),
-        "dateTo" : datetime.strptime("04.09.2016", "%d.%m.%Y"),
+        "dateTo" : datetime.strptime("03.09.2016", "%d.%m.%Y"),
 }
 
 res = p.keywords.createReport(USER, FILTER)
 print "Create report: with filter: %s " % FILTER
 
+
+clicks = impressions = 0
 while 1:
-	r = p.keywords.stats(USER, res["reportId"], 0, 1000)
+	r = p.keywords.stats(USER, res["reportId"], 0, 10000)
 	if r["status"] == 200:
 		for report in r["report"]:
-			print report
+			for line in report["stats"]:
+				clicks += line["clicks"]
+				impressions += line["impressions"]
+				print "ID: %d clicks: %d, impressions: %d"  % (report["id"], line["clicks"], line["impressions"])
 		break
 	else:
-		print r["status"]
+		print r
 		time.sleep(1)
+
+print "SUM clicks: %s, impression: %s" % (clicks, impressions)
+
 
