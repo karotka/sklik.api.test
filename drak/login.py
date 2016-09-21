3603279fc90e3edf2bb66987cdc0050e5ef16326
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2016-09-21 10:14:53 +0200

    login.py

--- drak/login.py
+++ drak/login.py
@@ -1,1 +1,12 @@
-asfdskjdbfsbdahojiddlmo
+import xmlrpclib
+
+p = xmlrpclib.ServerProxy('https://api.sklik.cz/drak/RPC2')
+result = p.client.login("username@domain", 'password')
+
+if result["status"] == 200:
+    ses = result["session"]
+else:
+    print result
+    raise
+
+
