a07517ebb191450d3aef94a1e1a2466ab862ffd4
Zdenek Philipp <zdenek.philipp@firma.seznam.cz>
2017-01-10 15:26:45 +0100

    keyword stat

--- drak/login.py
+++ drak/login.py
@@ -1,7 +1,7 @@
 import xmlrpclib
 
 p = xmlrpclib.ServerProxy('https://api.sklik.cz/drak/RPC2')
-result = p.client.login("zdenek.philipp@firma.seznam.cz", 'KArotka66155.')
+result = p.client.login("username@domain", 'password')
 
 if result["status"] == 200:
     ses = result["session"]
